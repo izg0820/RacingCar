a77df025c13eb9fbbdbaa85cedd4816fb41029ea
이재현 <izg0820>
2022-05-25 15:56:04 +0900

    refactor: 테스트 수정

--- src/test/java/racingcar/model/ResultTest.java
+++ src/test/java/racingcar/model/ResultTest.java
@@ -1,32 +1,21 @@
 package racingcar.model;
 
 import org.junit.jupiter.api.Test;
-import racingcar.model.Cars;
-import racingcar.model.PlayResult;
-import racingcar.model.Repeat;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class ResultTest {
 
     @Test
-    void move테스트() {
-        Cars cars = new Cars("a,b,c");
-        cars.move();
-        for (Integer position : cars.getPositions()) {
-            assertThat(position).isBetween(0, 1);
-        }
-    }
-
-    @Test
     void 우승자찾기() {
-        Cars cars = new Cars("c,b,a");
+        Cars cars = new Cars("c,b");
         Repeat repeatTime = new Repeat("2");
         while (!repeatTime.isEnd()) {
-            cars.move();
+            cars.getCars().get(0).move(4);
+            cars.getCars().get(1).move(3);
             repeatTime.reduce();
         }
-        PlayResult result = cars.findWinner();
-        assertThat(result.resultSetting()).isLessThanOrEqualTo("c,b,a");
+        PlayResult result = new PlayResult(cars);
+        assertThat(result.getWinners()).isEqualTo("c");
     }
 }
